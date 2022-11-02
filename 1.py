f2ee152b9416b058d79155c5e88011382830d6eb
akcaau <gllazrv@gmail.com>
2022-11-02 17:11:02 +0300

    Исправлена первая задача после ревью

--- 1.py
+++ 1.py
@@ -1,29 +1,29 @@
-"""id = 73439610."""
+"id = 73653079."
 
 
 def broken_search(nums, target) -> int:
-    right = len(nums)
+    right = len(nums)-1
     left = 0
     first = nums[0]
     mid = (right + left) // 2
     if first == target:
         return 0
-    while right - left >= 2:
+    while right - left >= 1:
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
+        mid = (right + left) // 2
+        if nums[mid] == target:
+            return mid
         if first > target:
-            if nums[mid] == target:
-                return mid
             if nums[mid] > target and nums[mid] < nums[0]:
-                right = mid
+                right = mid - 1
             else:
-                left = mid
-            mid = (right + left) // 2
+                left = mid + 1
         else:
-            if first < target:
-                if nums[mid] == target:
-                    return mid
             if nums[mid] < target and nums[mid] > nums[0]:
-                left = mid
+                left = mid + 1
             else:
-                right = mid
-            mid = (right + left) // 2
+                right = mid - 1
     return -1
