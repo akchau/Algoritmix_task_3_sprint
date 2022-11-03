34485b37d76006d7a82cb8fbd5764f09e132bc1d
akcaau <gllazrv@gmail.com>
2022-11-04 01:13:27 +0300

    Исправил окончательно первую задачу

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-"id = 73653079."
+"id = 73775272."
 
 
 def broken_search(nums, target) -> int:
@@ -12,12 +12,12 @@ def broken_search(nums, target) -> int:
         if nums[mid] == target:
             return mid
         if first > target:
-            if nums[mid] > target and nums[mid] < nums[0]:
+            if nums[0] > nums[mid] > target:
                 right = mid - 1
             else:
                 left = mid + 1
         else:
-            if nums[mid] < target and nums[mid] > nums[0]:
+            if nums[0] < nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
