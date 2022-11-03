63a8261c990dc0dcc13a55930bc2ad574de99caf
akcaau <gllazrv@gmail.com>
2022-11-04 01:03:52 +0300

    Исправил все замечания во второй задаче

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-"""id = 73689835."""
+"""id = 73775017."""
 
 
 class User:
@@ -8,47 +8,45 @@ class User:
         self.fine = fine
 
     def __lt__(self, other):
-        if int(self.tasks) > int(other.tasks):
-            return True
-        elif int(self.tasks) == int(other.tasks):
-            if int(self.fine) < int(other.fine):
-                return True
-            elif int(self.fine) == int(other.fine):
-                if self.name < other.name:
-                    return True
-        return False
+        self_t = (self.tasks, self.fine, self.name)
+        other_t = (other.tasks, other.fine, other.name)
+        return self_t < other_t
 
     def __str__(self):
         return self.name
 
 
-def quicksort_inplace(array, left, right):
+def quicksort(array, left, right):
     if left >= right:
         return
     left_idx = left
     right_idx = right
-
-    pivot_idx = (left + right) // 2
-    pivot = array[pivot_idx]
+    pivot = array[(left+right)//2]
     while left_idx < right_idx:
-        while array[left_idx].__lt__(pivot):
+        while array[left_idx] < pivot:
             left_idx += 1
-        while pivot.__lt__(array[right_idx]):
+        while pivot < array[right_idx]:
             right_idx -= 1
         if left_idx <= right_idx:
             array[left_idx], array[right_idx] = array[right_idx], array[left_idx]
             left_idx += 1
             right_idx -= 1
-    quicksort_inplace(array, left, right_idx)
-    quicksort_inplace(array, left_idx, right)
+    quicksort(array, left, right_idx)
+    quicksort(array, left_idx, right)
+
+
+def transformation(user_0, user_1, user_2):
+    user_1 = - int(user_1)
+    user_2 = int(user_2)
+    return [user_0, user_1, user_2]
 
 
 def main():
     user_num = int(input())
     user = [
-        User(*input().split()) for _ in range(user_num)
+        User(*transformation(*input().split())) for _ in range(user_num)
     ]
-    quicksort_inplace(user, 0, user_num - 1)
+    quicksort(user, 0, user_num - 1)
     print(*user, sep='\n')
 
 
